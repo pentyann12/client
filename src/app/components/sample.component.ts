52945576ab95377dd72b73ab4e75771566c80bbc
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2025-09-06 18:04:04 +0900

    Drag可能に

--- src/app/components/sample.component.ts
+++ src/app/components/sample.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {CdkDrag} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-sample',
-  template: `<div>動的コンポーネントが生成されました！</div>`,
-  standalone: true
+  template: `<div cdkDrag>動的コンポーネントが生成されました！</div>`,
+  standalone: true,
+  imports: [CdkDrag],
 })
 export class SampleComponent {}
