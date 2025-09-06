6d68dfe570c194d0bc052aec170ee6720459eca6
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2025-09-06 19:29:35 +0900

    整列可能に

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SampleComponent } from './components/sample.component';
+import { ComponentRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,21 @@ export class App {
   @ViewChild('container', { read: ViewContainerRef, static: true })
   container!: ViewContainerRef;
 
+  private componentRefs: ComponentRef<SampleComponent>[] = [];
+
   createDynamicComponent(top: number = 0, left: number = 0) {
     const ref = this.container.createComponent(SampleComponent);
-    ref.instance.top = top;
-    ref.instance.left = left;
-    ref.instance.close.subscribe(() => ref.destroy());
+    ref.instance.position = {x: left, y: top};
+    ref.instance.close.subscribe(() => {
+      ref.destroy();
+      this.componentRefs = this.componentRefs.filter(r => r !== ref);
+    });
+    this.componentRefs.push(ref);
+  }
+
+  alignComponents() {
+    this.componentRefs.forEach((ref, i) => {
+      ref.instance.position = {x: i * 50, y: i * 50};
+    });
   }
 }
--- src/app/components/sample.component.ts
+++ src/app/components/sample.component.ts
@@ -1,22 +1,24 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CdkDrag } from '@angular/cdk/drag-drop';
-import { NgStyle } from '@angular/common';
 
 import { Input } from '@angular/core';
 @Component({
   selector: 'app-sample',
   template: `
-    <div cdkDrag [ngStyle]="{'top.px': top, 'left.px': left, 'position': 'absolute'}">
+    <div cdkDrag [cdkDragFreeDragPosition]="position" class="sample-component">
       <button (click)="close.emit()">×</button>
-      <div>動的コンポーネントが生成されました！</div>
+      <div>動的コンポーネント</div>
     </div>
   `,
   standalone: true,
-  imports: [CdkDrag, NgStyle],
-
+  imports: [CdkDrag],
+  styles: [`
+    .sample-component {
+      position: absolute;
+    }
+  `]
 })
 export class SampleComponent {
-  @Input() top: number = 0;
-  @Input() left: number = 0;
+  @Input() position = { x: 0, y: 0 };
   @Output() close = new EventEmitter<void>();
 }
